9ae5bb76bbd161dac7bd24b2f542973c6796c058
JuanCamiloPaezArizaCAMPUS <camipa2305@gmail.com>
2023-08-17 07:55:08 -0500

    feat: :sparkles: Se agregan los endpoints creados al archivo app.js para que se puedan utilizar los endpoints

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import dotenv from "dotenv";
 import express from "express";
 import Productos from "./router/productoRouter.js";
+import Inventario from "./router/inventarioRouter.js";
+import Bodega from "./router/bodegasRouter.js";
 import { limitRequest } from "./helpers/limitRequest.js";
 import { generateToken, verifyToken } from "./jwt/tokens.js";
 dotenv.config();
@@ -10,6 +12,8 @@ app.use(express.json());
 
 app.use("/token",limitRequest(),generateToken);
 app.use("/productos", limitRequest(),verifyToken,Productos);
+app.use("/inventario", limitRequest(),verifyToken,Inventario);
+app.use("/bodegas", limitRequest(),verifyToken,Bodega);
 
 
 let config = JSON.parse(process.env.MY_SERVER);
