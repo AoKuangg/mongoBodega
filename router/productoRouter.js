de3e34050c3d03f4629178d9a08a325913aaf6c8
JuanCamiloPaezArizaCAMPUS <camipa2305@gmail.com>
2023-08-17 07:35:10 -0500

    feat: :sparkles: Se agrega el post para el proyecto de mongoBodegas tambien se comenta un  pequeño ejemplo del post

--- router/productoRouter.js
+++ router/productoRouter.js
@@ -22,4 +22,39 @@ Productos.get("/", ProductoVerify ,async (req, res) => {
       }
 });
 
+Productos.post("/", ProductoVerify, ProductoMiddleware, async (req, res) => {
+  try {
+    const nuevoID = await autoIncrementID("products");
+    let db = await connDB();
+    let coleccion = db.collection("products");
+    await coleccion.insertOne({
+      ID: nuevoID,
+      ...req.body,
+    });
+    res.status(200).send({status:200, message:"Dato correctamente insertado"});
+  } catch (error) {
+    res.status(500).send({
+      status: 500,
+      errorInfo: {
+        message: "Error al insertar los datos",
+        error: error.message,
+      },
+    });
+  }
+
+  /**
+   * *Ejemplo para la insercion de datos
+   */
+    // {
+  //   "product_name": "Blue Label",
+  //   "product_description": "Es un elixir",
+  //   "product_state": "activo",
+  //   "product_created_by":1,
+  //   "product_updated_by":null,
+  //   "product_created_at": "2023-08-17",
+  //   "product_updated_at":null,
+  //   "product_deleted_at": null
+  // }
+});
+
 export default Productos;
